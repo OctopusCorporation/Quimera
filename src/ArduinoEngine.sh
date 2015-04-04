927d9f4e02c3d300e8440c40ad5bac9ae506235a
Cami Rodriguez <camiepisode@outlook.com>
2015-04-04 00:39:59 -0500

    test

--- src/ArduinoEngine.sh
+++ src/ArduinoEngine.sh
@@ -4,7 +4,7 @@ appName=''
 arduinoId=''
 
 # Read all flags and get the parameters
-if ! options=$(getopt -o ni: -l appName,arduinoId,clong: -- "$@")
+if ! options=$(getopt -o n:i: -l appName,arduinoId,clong: -- "$@")
 then
     # something went wrong, getopt will put out an error message for us
     exit 1
@@ -14,16 +14,19 @@ set -- $options
 while true; do
   case $1 in
     -n|--appName)
-      				appName="$2"
-      				shift
+echo " opcion -n"
+      				#appName="$2"
+      				#shift
       	;;
     -i|--arduinoId)
-					arduinoId="$2"
-					shift
+echo " opcion -i"
+					#arduinoId="$2"
+					#shift
 	  	;;
     (--) shift; break;;
     (-*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
-    (*) break;;
+    (*) 
+echo " opcion desconocida";break;;
   esac
   shift
 done
