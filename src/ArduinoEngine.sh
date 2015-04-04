10bf10685d7b492408614139e3941fb055fe49ab
Cami Rodriguez <camiepisode@outlook.com>
2015-04-03 23:43:17 -0500

    Corrected variables

--- src/ArduinoEngine.sh
+++ src/ArduinoEngine.sh
@@ -7,13 +7,13 @@ arduinoId=''
 inputOptions='getopt -o ni: --long appName,arduinoId:'
 eval set -- inputOptions
 while true; do
-  case $opt in
+  case "$1" in
     --appName | -n)
-      				appName=$2
+      				appName="$2"
       				shift
       	;;
     --arduinoId | -i)
-					arduinoId=$2
+					arduinoId="$2"
 					shift
 	  	;;
     *)
